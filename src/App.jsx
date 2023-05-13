774cb23a2e0929a1dc8b3d679fa2050d480d3cee
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2023-05-14 06:43:21 +0700

    feat: added variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.scss';
-import Header from '../Header/Header';
-import Form from '../Form/Form';
-import Popup from '../Popup/Popup';
+import Header from './components/Header/Header';
+import Form from './components/Form/Form';
+import Popup from './components/Popup/Popup';
 
 function App() {
   const [popupOpened, setPopupOpened] = useState(false);
@@ -12,7 +12,7 @@ function App() {
     setPopupOpened(true);
     setUrl(e.target[0].value);
   };
-  
+
   return (
     <>
       <Header />
